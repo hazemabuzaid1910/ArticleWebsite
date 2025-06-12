a03cbb91f0b09b6904e6085974f78ffa5b086823
hazemabuzaid <hazemabuzaid16@gmail.com>
2025-06-12 22:49:40 +0300

    person image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -282,7 +282,7 @@ const writer = [
      <Link to="#" className='bg-white w-fit text-[#CC522B] flex gap-2 items-center py-2 px-4'><span>Start Writing</span><FaArrowRight/></Link>
    </div>
     <div className='w-[100%] h-[100%] order-1 sm:order-2'>
-       <img src="/public/person.png" alt="" className='w-full h-full'/>
+       <img src="/person.png" alt="" className='w-full h-full'/>
     </div>
     </div> 
 <div className='flex flex-col gap-5 bg-[var(--card-bg)] sm:w-[50%] px-8 py-5'>
